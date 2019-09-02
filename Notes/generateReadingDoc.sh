213fe6fdcfb9fdd3663b015a8b09bf8be61e416c
zack <dzackgarza@gmail.com>
2019-09-01 23:03:54 -0400

    / ‘Notes/generateReadingDoc.sh’

--- Notes/generateReadingDoc.sh
+++ Notes/generateReadingDoc.sh
@@ -11,7 +11,7 @@ filenames=$(find "$1" -name "*.md" | sort)
 echo "" > $2
 
 for f in $filenames; do
-  echo "$f" >> $2
+  cat "$f" >> $2
 done
 
 #sed -i "1s/^/[toc]\n\n#Reading Notes\n\n/" ReadingNotes.md
